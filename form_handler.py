60cd72d4c1196806d805f133d8d81f803c4ff11c
Carla Monteiro <carla.monteiro@ds.feb.br>
2026-08-18 09:52:26 -0300

    Fix: Usar campo 'Comentário' com acento no SharePoint

--- form_handler.py
+++ form_handler.py
@@ -720,7 +720,7 @@ def obter_pesquisas():
             pesquisas.append({
                 'id': item.get('id'),
                 'avaliacao': fields.get('Avaliacao', ''),
-                'comentario': fields.get('Comentário', ''),  # CORRETO (sem S, com acento)
+                'comentario': fields.get('Comentário', ''),  # Com ACENTO/TIL
                 'numeroChamado': fields.get('NumeroChamado', ''),
                 'dataResposta': fields.get('Created', datetime.now().isoformat()),
             })
